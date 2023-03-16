e872b30c1ed2e03aec619c065b4a5598c2c3a96e
Aryan Goel <aryangoel6900@yahoo.in>
2023-03-16 17:44:16 +0530

    feat: middleware that protects admin routes

--- src/middlewares/roleValidation.js
+++ src/middlewares/roleValidation.js
@@ -1,3 +1,32 @@
-const roleValidationMiddleware = (req, res, next) => {
-  // TODO:  
+const { managementPrisma } = require('../prismaClient');
+const { convertToRoleEnum } = require('../utils/managementDbUtils');
+
+const roleValidationMiddleware = async (req, res, next) => {
+  // TODO:
+  // const { email } = req.user;
+  const email = 'cricket@email.com'; // dummy
+  const { projectId } = req.params;
+  
+  const isAdmin = await managementPrisma.projectMember.findUnique({
+    where: {
+      projectId_email: {
+        projectId: parseInt(projectId),
+        email,
+      },
+    },
+    select: {
+      role: true,
+    },
+  });
+
+  if (isAdmin.role === convertToRoleEnum('ADMIN')) {
+    next();
+  }
+  else {
+    res.status(403).json({ message: 'You cannot access these services.' });
+  }
+};
+
+module.exports = { 
+  roleValidationMiddleware 
 };

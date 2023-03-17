02c2b91099055551bbea53830f70556078a71a0b
Aryan Goel <aryangoel6900@yahoo.in>
2023-03-18 01:14:10 +0530

    refactor: add member id to po notes

--- prisma/dashboard-db/migrations/20230317193627_memberid_ponote/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `memberId` to the `PONote` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "PONote" ADD COLUMN     "memberId" INTEGER NOT NULL;
